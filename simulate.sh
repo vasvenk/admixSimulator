c30520ce399e0d50dbe71ba376a6bd3331ea5c8c
vasvenk <vas.venk@berkeley.edu>
2019-01-19 10:47:50 -0800

    Changed setup

---
+++ simulate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=DatesTest
+#
+# Account:
+#SBATCH --account=fc_moorjani
+#
+# Partition:
+#SBATCH --partition=savio2
+#
+# Wall clock limit:
+#SBATCH --time=00:30:00
+#
+## Command(s) to run:
